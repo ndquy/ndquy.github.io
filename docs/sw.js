03b1d254f010306a1099e3f16e92f870be65a381
dinhquy94 <dinhquy94@gmail.com>
2021-04-03 16:45:49 +0700

    edit  post

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210403.1742';
+var cacheName = 'chirpy-20210403.1744';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
