f6ca3fa8f1acf767a0fa35acc015c40bc260f44e
quynd <dinhquy94@gmail.com>
2021-09-09 15:19:14 +0700

    build to prod

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210509.1405';
+var cacheName = 'chirpy-20210909.1607';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
